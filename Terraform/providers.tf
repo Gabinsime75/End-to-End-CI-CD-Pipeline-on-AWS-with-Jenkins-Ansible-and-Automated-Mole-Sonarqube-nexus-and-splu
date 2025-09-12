1e1e20f216c3454975d8cf0c5b033820ac5315d2
Gabin Sime <gabinsime@yahoo.com>
2025-09-11 22:06:44 -0500

    Installation files added

---
+++ Terraform/providers.tf
@@ -0,0 +1,17 @@
+# Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+required_version = "~> 1.12.0" 
+
+}  
+
+# Provider Block
+provider "aws" {
+  region = "us-east-1"
+  
+}
